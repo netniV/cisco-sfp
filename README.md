74f6185012ceced77c7372e337b7609578449908
netniV <netniV@hotmail.com>
2017-12-19 10:18:21 +0000

    Version 0.2.3
    Code for 0.2.3 version of cisco-sfp

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# cisco-sfp
+# cisco-sfp for cacti 0.8.x
 This is to host code for https://forums.cacti.net/viewtopic.php?f=19&amp;t=23089 for others
+
+Cisco SFP statistics for cacti (c) 2007-2008 sodium
+
+This code is compatibile with cacti 0.8.x.  There may be issues with the 1.x versions of cacti.
+
+## INSTALLATION :
+1. copy cisco_sfp.xml to [$cacti_home]/resources/script_server directory
+2. copy ss_65xx_sfp.php to [$cacti_home]/scripts directory
+3. import the following template (within cacti) `cacti_data_query_cisco_65xx_sfp_statistics.xml`
+4. add Cisco - 65xx - SFP statistics to your catalyst host template or add it to the "Associated Data Queries" in the devices list.
+
+That's it...
+
+## THOLD plugin compatibility
+
+Note: if your are running the thold plugin you should patch it (thold baseline function can't handle negative numbers (you do NOT have to patch if you only do high/low watermark checking.
+
+I have included the path file for thold. (v0.3.9) to appy patch: cacti@nms:/usr/share/cacti/plugins/thold$ patch -p0 < thold_functions-0.3.9.patch
+
+Assumptions made within the code:
+- When the script can not read the value from the SFP -40 (dBm) is returned
+- snmp_port = 161
+- snmp_timeout = 5000 ms
---
+++ ss_65xx_sfp.php
@@ -0,0 +1,123 @@
+<?php
+# vim:ts=2:set noai:
+/*
+ +-------------------------------------------------------------------------+
+ | Copyright (C) 2007 sodium                                               |
+ |                                                                         |
+ | This program is free software; you can redistribute it and/or           |
+ | modify it under the terms of the GNU General Public License             |
+ | as published by the Free Software Foundation; either version 2          |
+ | of the License, or (at your option) any later version.                  |
+ |                                                                         |
+ | This program is distributed in the hope that it will be useful,         |
+ | but WITHOUT ANY WARRANTY; without even the implied warranty of          |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           |
+ | GNU General Public License for more details.                            |
+ +-------------------------------------------------------------------------+
+ | name    : ss_65xx_sfp.php                                               |
+ | version : 0.2.1                                                         |
+ | date    : 20070910                                                      |
+ +-------------------------------------------------------------------------+
+*/
+$no_http_headers = true;
+error_reporting(E_ERROR);
+
+include_once(dirname(__FILE__) . "/../lib/snmp.php");
+
+if (!isset($called_by_script_server)) {
+	include_once(dirname(__FILE__) . "/../include/config.php");
+	array_shift($_SERVER["argv"]);
+	print call_user_func_array("ss_sfp", $_SERVER["argv"]);
+
+}
+
+# PortIndex 			1.3.6.1.2.1.47.1.1.1.1.2
+# entSensorValue 	1.3.6.1.4.1.9.9.91.1.1.1.1.4 (RX = .4.[index] TX =.4.[index] + 1)
+# entSensorStatus 1.3.6.1.4.1.9.9.91.1.1.1.1.5 (1 = ok, 2 = unavailable, 3 = nonoperational)
+# entSensorType 	1.3.6.1.4.1.9.9.91.1.1.1.1.1 (output type 14 = in dBm)
+#
+# /usr/bin/php -q /usr/share/cacti/scripts/ss_65xx_sfp.php 192.168.1.11 public 2 index
+# /usr/bin/php -q /usr/share/cacti/scripts/ss_65xx_sfp.php 192.168.1.11 public 2 query index|status|descr
+# /usr/bin/php -q /usr/share/cacti/scripts/ss_65xx_sfp.php 192.168.1.11 public 2 get rx|tx TenGigabitEthernet1/1
+# 
+function ss_sfp($hostname, $snmp_community, $snmp_version, $cmd, $direction = "", $interface = "") {
+
+	$result = "";
+	$oid_name = "";
+  $sensor_name = "";
+  $sensor_status = "";
+	$sensor_string = "";
+	$status_string = "";
+	$tx_status = 0;
+	$int = "";
+	$snmp_retries = read_config_option("snmp_retries");
+	$var = (cacti_snmp_walk($hostname, $snmp_community, ".1.3.6.1.4.1.9.9.91.1.1.1.1.1", $snmp_version, "", "", 161, 5000, $snmp_retries));
+
+	if ($cmd == "index" || $cmd == "query") {
+		for ($i=0;$i<(count($var));$i++) {
+			if ($var[$i]["value"] == "14") {                // found a dBm entry
+				$sensor_name = (cacti_snmp_get($hostname, $snmp_community, ereg_replace('.*\.[0-9]+\.[0-9]+\.([0-9]+)$', '.1.3.6.1.2.1.47.1.1.1.1.2.\\1', $var[$i]["oid"]), $snmp_version, "", "", 161, 5000, $snmp_retries));
+				$sensor_status = (cacti_snmp_get($hostname, $snmp_community, ereg_replace('.*\.[0-9]+\.[0-9]+\.([0-9]+)$', '.1.3.6.1.4.1.9.9.91.1.1.1.1.5.\\1', $var[$i]["oid"]), $snmp_version, "", "", 161, 5000, $snmp_retries));
+      	preg_match("/[^\ ]+/", $sensor_name, $oid_name); // don't care about the rest of the string
+				if ($cmd == "index") {
+      		print $oid_name[0]."\n";
+				} elseif ($cmd == "query") {
+					switch ($direction) {
+						case "index":
+							print $oid_name[0].":".$oid_name[0]."\n";
+							$i=$i+1;	// skip rx
+						break;
+						case "status":
+							if ($tx_status == 0) { 
+								if ($sensor_status == 1) { 
+									$status_string = "Online"; 
+								} else { 
+									$status_string = "Tx failure"; 
+								}
+								$tx_status = 1; 
+							} else { 
+								if ($sensor_status != 1) { 
+									if ($status_string == "Online") {
+										$status_string = "Rx failure";	
+									} else {
+										$status_string = $status_string . " Rx failure";
+									}
+								}
+								$tx_status = 0; 
+								print $oid_name[0].":".$status_string."\n"; 
+							}
+						break;
+						case "descr":
+							$host_id = db_fetch_cell("select id from host where hostname = '$hostname'");
+							$snmp_index = db_fetch_cell("select snmp_index from host_snmp_cache where host_id = '$host_id' and field_value = '$oid_name[0]' and field_name = 'ifDescr'");
+							$alias = db_fetch_cell("select field_value from host_snmp_cache where snmp_index='$snmp_index' and host_id='$host_id' and field_name='ifAlias'");
+							print $oid_name[0] . ":" . $alias ."\n";
+							$i=$i+1;  // rx & tx description are the same
+						break;
+					}
+				}
+			}
+		}
+	} elseif ($cmd == "get") {
+		for ($i=0;$i<(count($var));$i++) {
+			if ($var[$i]["value"] == "14") {                // found a dBm entry
+				$sensor_name = (cacti_snmp_get($hostname, $snmp_community, ereg_replace('.*\.[0-9]+\.[0-9]+\.([0-9]+)$', '.1.3.6.1.2.1.47.1.1.1.1.2.\\1', $var[$i]["oid"]), $snmp_version, "", "", "", 5000, $snmp_retries));
+				if ($direction == "tx") { 
+					$int=$interface." Transmit Power Sensor"; 
+				} elseif ($direction == "rx") { 
+					$int=$interface." Receive Power Sensor"; 
+				}
+				preg_match("/[^\ ]+/", $sensor_name, $oid_name);
+				if (strstr($int, $sensor_name)) {
+					if (cacti_snmp_get($hostname, $snmp_community, ereg_replace('.*\.[0-9]+\.[0-9]+\.([0-9]+)$', '.1.3.6.1.4.1.9.9.91.1.1.1.1.5.\\1', $var[$i]["oid"]), $snmp_version, "", "", 161, 5000, $snmp_retries) == "1") {
+						$result = (cacti_snmp_get($hostname, $snmp_community, ereg_replace('.*\.[0-9]+\.[0-9]+\.([0-9]+)$', '.1.3.6.1.4.1.9.9.91.1.1.1.1.4.\\1', $var[$i]["oid"]), $snmp_version, "", "", 161, 5000, $snmp_retries))/10;
+					}
+					else { $result = "-40"; // lights are off
+					}
+				}
+			}
+		}
+	}
+return trim($result);
+}
+?>
